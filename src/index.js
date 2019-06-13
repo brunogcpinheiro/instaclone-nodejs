235e8e97d3b5507a34b57fc824530779246a26ae
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-06-13 18:09:20 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	return res.send("Ola Mundo");
+});
+
+app.listen(3333, () => console.log("Server running...."));
